c5bb6e32a7c86376122a7c0c7a487f94f213741a
salpreh <salva.perez46@gmail.com>
2022-08-02 13:29:14 +0200

    DB entity templates and fragments
    - Registered templates in config

---
+++ crudgen/adapters/infrastructure/template_writer/config/tempate_config.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+# Pkg
+TEMPLATES_PACKAGE = 'crudgen.adapters.infrastructure.template_writer'
+
+# Base folders
+TEMPLATE_FOLDER = 'templates'
+DB_FOLDER = 'db'
+COMMONS_FOLDER = 'commons'
+FRAGMENTS_FOLDER = 'fragments'
+
+# DB TEMPLATES
+DB_ENTITY_TEMPLATE = 'entity.jinja2'
+
+# DB FRAGMENTS
+COLLECTION_INVERSE_METHODS = 'collection_inverse_methods.jinja2'
+COLLECTION_MAIN_METHODS = 'collection_main_methods.jinja2'
+FIELD = 'field.jinja2'
+ID_FIELD = 'id_field.jinja2'
+M2M_INVERSE_FIELD = 'm2m_inverse_field.jinja2'
+M2M_MAIN_FIELD = 'm2m_main_field.jinja2'
+M2ONE_FIELD = 'm2one_field.jinja2'
+ONE2M_FIELD = 'one2m_field.jinja2'
+
+_FRAGMENTS = [
+    COLLECTION_INVERSE_METHODS,
+    COLLECTION_MAIN_METHODS,
+    FIELD,
+    ID_FIELD,
+    M2M_INVERSE_FIELD,
+    M2M_MAIN_FIELD,
+    M2ONE_FIELD,
+    ONE2M_FIELD
+]
+
+
+def get_db_file_path(file_name: str) -> str:
+    file_path = Path(DB_FOLDER)
+    if _is_fragment(file_name): file_path = file_path / FRAGMENTS_FOLDER
+
+    return str(file_path / file_name)
+
+
+def _is_fragment(file_name: str) -> bool:
+    return file_name in _FRAGMENTS
