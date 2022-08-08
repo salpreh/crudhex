0e2c9e9d33db236910ab93a7eda355afa73c240c
salpreh <salva.perez46@gmail.com>
2022-08-08 19:13:03 +0200

    Domain model template

--- crudhex/adapters/infrastructure/template_writer/services/template_env.py
+++ crudhex/adapters/infrastructure/template_writer/services/template_env.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 from jinja2 import Environment, PackageLoader
 
+from ..config import tempate_config
 
 _TEMPLATE_ENV: Optional[Environment] = None
 
@@ -10,7 +11,7 @@ def get_template_environment():
     global _TEMPLATE_ENV
     if _TEMPLATE_ENV: return _TEMPLATE_ENV
 
-    _TEMPLATE_ENV = Environment(loader=PackageLoader('crudhex.adapters.infrastructure.template_writer', 'templates'),
+    _TEMPLATE_ENV = Environment(loader=PackageLoader(tempate_config.TEMPLATES_PACKAGE, tempate_config.TEMPLATE_FOLDER),
                                 trim_blocks=True, lstrip_blocks=True)
 
     return _TEMPLATE_ENV
