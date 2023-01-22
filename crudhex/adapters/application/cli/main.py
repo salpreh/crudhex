e220276cd7709a594a8d7a05dc168c244bb76f6f
salpreh <salva.perez46@gmail.com>
2023-01-22 20:25:25 +0100

    Revert "Refactor to separated sub commands"
    This reverts commit 753116b82de3b98991623a91a5d7d88d468ebd3c.

--- crudhex/adapters/application/cli/main.py
+++ crudhex/adapters/application/cli/main.py
@@ -5,7 +5,18 @@ from rich.console import Console
 from rich.theme import Theme
 
 from . import console_out_context as console_context
-from .commands import generate_crud
+from .commands.generate_crud import generate as generate_crud
+from crudhex.domain.models.mapper import MapperType
+from crudhex.domain.services import config_context
+
+_SPEC_HELP = 'Spec file path to process'
+_CONF_HELP = f'Project config file to know packages and code paths. Defaults to {config_context.DEFAULT_CONFIG}'
+_FORCE_HELP = f'Override file outputs if exists'
+_MAPPER_HELP = f'Mapper generation option. By default no mapper will be generated.' \
+               '\n[WARN]: modelmapper generation not supported yet.'
+_API_MODELS_HELP = 'Generate API models'
+_API_PAGE_HELP = 'Use Spring data Page as return type for get all endpoints. By default List with items is returned'
+_EXCEPTION_HANDLER_HELP = 'Generate a default exception handler for Spring REST controllers (ControllerAdvice)'
 
 app = typer.Typer()
 out_console: Optional[Console] = None
@@ -14,12 +25,23 @@ err_console: Optional[Console] = None
 
 def main():
     _setup()
-    _enable_commands()
     app()
 
 
-def _enable_commands():
-    app.add_typer(generate_crud.app, name='generate', help='Generate CRUD classes from a spec file')
+@app.command(help='Generate CRUD classes from a spec file')
+def generate(
+        spec_file: str = typer.Argument(..., help=_SPEC_HELP),
+        project_config: str = typer.Option(None, '--config', '-c', help=_CONF_HELP),
+        force_override: bool = typer.Option(False, '--force', '-f', help=_FORCE_HELP),
+        mapper_type: MapperType = typer.Option(MapperType.NONE.value, '--mapper', '-m', help=_MAPPER_HELP),
+        gen_api_models: bool = typer.Option(True, '--generate-api-models/--no-generate-api-models', help=_API_MODELS_HELP),
+        with_api_page: bool = typer.Option(False, '--with-api-page', '-ap', help=_API_PAGE_HELP),
+        add_exception_handler: bool = typer.Option(True, '--add-exception-handler/--no-add-exception-handler', help=_EXCEPTION_HANDLER_HELP)
+):
+
+    generate_crud(spec_file, project_config, force_override,
+                  mapper_type, gen_api_models, with_api_page,
+                  add_exception_handler)
 
 
 def _setup():
